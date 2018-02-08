ffda91bdc562a011d06de07aeb4a86e9818ab8aa
Adam Nelson <anelson@apocryph.org>
2018-02-08 17:12:29 -0500

    Dramatically improve polybar fonts and information content

--- home/.config/polybar/processor-clock-speed.sh
+++ home/.config/polybar/processor-clock-speed.sh
@@ -6,4 +6,13 @@
 # Enumerate the 'sys filesystem to get all of the 
 # 'scaling_cur_freq' files, read them into an array
 # and somehow compute the max
+frequency_files=$(find /sys/devices/system/cpu/cpufreq -name scaling_cur_freq)
+cpu_frequencies=$(cat $frequency_files)
+max_frequency=$(echo "$cpu_frequencies" | sort -nr | head -n1)
+
+# the frequencies are always reported in kilohertz
+# I want to pretty-print them
+max_ghz=$(echo "$max_frequency / 1024 / 1024" | bc -l)
+printf "%0.2f" $max_ghz
+
 
