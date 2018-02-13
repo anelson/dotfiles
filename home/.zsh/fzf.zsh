969ad3c2fd09fed2f9e1dcf0b2b94b52310780f6
Adam Nelson <anelson@apocryph.org>
2018-02-13 11:27:40 -0500

    [fzf] by default fzf should follow symlinks

--- home/.zsh/fzf.zsh
+++ home/.zsh/fzf.zsh
@@ -11,7 +11,7 @@ fi
 # use 'fd' to find files and directories since it's much faster and respects both .gitignore and .ignore
 export FZF_TMUX=1  # use tmux by default
 export FZF_DEFAULT_OPTS="--history=$HOME/.fzf_history --ansi"
-export FZF_DEFAULT_COMMAND='fd --type file --color=always --hidden --exclude .git'
+export FZF_DEFAULT_COMMAND='fd --type file --color=always --hidden --follow --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="fd --type directory --color=always --hidden --exclude .git"
+export FZF_ALT_C_COMMAND="fd --type directory --color=always --hidden --follow --exclude .git"
 
