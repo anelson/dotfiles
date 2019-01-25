d3a30a8b8ba5a3c3ed4694f3e3305e1e0e69f4e2
Adam Nelson <anelson@users.noreply.github.com>
2019-01-25 14:21:04 +0200

    [bin] add script to run the correct Rust RLS, respecting local dir overrides

---
+++ home/.local/bin/run-rust-rls.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Wrapper script to run the rust language server, ensuring that the correct version is run.
+#
+# Usually I want the stable toolchain and the stable RLS.
+#
+# But sometimes I have projects using nightly and in that case I want to use nightly RLS.
+#
+# But sometimes nightly is broken, so I use some slightly-older archived version of nightly, and its RLS
+#
+# In that case I use `rustup override set (toolchain)` in the directory of a project to force a specific roolchain.
+#
+# This script runs the RLS corresponding to the overridden toolchain of the current directory, if applicable, otherwise
+# it runs the stable RLS.
+
+toolchain=$(rustup show active-toolchain)
+rustup run $toolchain rls
