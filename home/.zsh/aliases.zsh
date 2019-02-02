4afdaaf7c1205f7894cac3558cd8af7b9fb6441f
Adam Nelson <anelson@users.noreply.github.com>
2019-02-02 12:30:44 +0200

    [zsh] add aliases for ls that use exa if it's present

--- home/.zsh/aliases.zsh
+++ home/.zsh/aliases.zsh
@@ -27,3 +27,38 @@ fi
 
 # Sometimes I trust a host enough to pass through agent auth
 alias ssha=ssh -A
+
+# If exa is present use it instead of ls
+# There are two possible exa variants:
+# The standard exa from https://github.com/ogham/exa, and the modified one from the
+# pull request at https://github.com/ogham/exa/pull/368.  Hopefully someday that PR is merged
+# and this distinction isn't needed.
+#
+# Until that day we deal with this.  The PR adds an `--icons` option which inserts devicons in the
+# output.  I like that and want to use it if it's present
+if command -v exa >/dev/null 2>&1; then
+  # exa is present.  good, that's a start.
+  if exa --icons >/dev/null 2>&1; then
+    icons="--icons"
+  else
+    icons=""
+  fi
+
+  alias ls="exa $icons"
+  alias ll="exa -l $icons"
+  alias lll="exa -l $icons | less"
+  alias lla="exa -la $icons"
+  alias llt="exa -T $icons"
+  alias llfu="exa -bghHliS --git $icons"
+else
+  alias ls="ls --color=auto"
+  alias ll="ls -l --color=auto"
+  alias lll="ls -l --color=auto | less"
+  alias lla="ls -la --color=auto"
+  alias llfu=lla
+fi
+
+alias la=lla
+alias llt1="llt -L 1"
+alias llt2="llt -L 2"
+alias llt3="llt -L 3"
