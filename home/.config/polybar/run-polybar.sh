b8e013b0182af15537cc23025f9ccc349d0a565d
Adam Nelson <anelson@users.noreply.github.com>
2019-03-22 16:20:03 +0200

    [polybar] fix tray size and inconsistent position
    Polybar can only display the tray on one monitor; which one gets it is
    basically a race condition.  I've changed the launch script now to
    always place the tray on the first monitor in the `xrandr` output.
    
    I've also set the tray size to `32` so the icons are much more legibile
    on a HiDPI screen

--- home/.config/polybar/run-polybar.sh
+++ home/.config/polybar/run-polybar.sh
@@ -9,8 +9,11 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch polybar on all displays
 active_monitors=( $(xrandr | grep " connected" | cut -d ' ' -f1) )
 
+tray_position=
 for mon in "${active_monitors[@]}"; do
-  MONITOR="$mon" polybar top &
+  MONITOR="$mon" TRAY_POSITION="$tray_position" polybar top &
+  # The tray should appear only on the first monitor, so set the tray position to none for all subsequent monitors
+  tray_position="none"
 done
 
 echo "Bars launched..."
