ca4dbb32e6cdc19375542c51c00528514bd29b64
Adam Nelson <anelson@users.noreply.github.com>
2021-08-18 20:17:51 +0300

    [fzf][tmux] Use the new tmux popup feature in the shell
    When using fzf in the shell, like Ctrl-R to search command history, add
    `-p` to the tmux command line.  Very new versions of tmux, like 3.2a (I
    had to build from source on Fedora 32, it's not yet packaged) support a
    popup window.  Sweet!
    
    This also modifies the `ftwind` script which I use to switch between
    tmux windows with fzf, to also use the popup window functionality.

--- home/.zsh/fzf.zsh
+++ home/.zsh/fzf.zsh
@@ -18,6 +18,7 @@ fi
 # Better FZF
 # use 'fd' to find files and directories since it's much faster and respects both .gitignore and .ignore
 export FZF_TMUX=1  # use tmux by default
+export FZF_TMUX_OPTS="-p"  # tmux popup!?  LOLWUT?
 export FZF_DEFAULT_OPTS="--history=$HOME/.fzf_history --ansi"
 export FZF_DEFAULT_COMMAND='fd --type file --color=always --hidden --follow --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
