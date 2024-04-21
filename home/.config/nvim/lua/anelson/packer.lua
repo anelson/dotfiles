69befdc5211506418aa1d077eb2b8dcda5abc72d
Adam Nelson <anelson@users.noreply.github.com>
2024-04-21 18:27:25 +0200

    [vim] Remove a copilot key binding that broke Esc in insert mode
    This is not new, I do not know why this started to be a problem only
    now.  But I accidentally put a key binding for `<C-]>` which is also
    Escape.  I didn't notice for a long time, until suddenly after I did a
    `PackerSync`, escape wasn't working!

--- home/.config/nvim/lua/anelson/packer.lua
+++ home/.config/nvim/lua/anelson/packer.lua
@@ -89,8 +89,8 @@ return packer.startup(function(use)
   -- Replace the programmer at the keyboard with an AI copilot
   use { "github/copilot.vim",
     config = function()
-      vim.keymap.set('i', '<C-]>', '<Plug>(copilot-next)')
-      vim.keymap.set('i', '<C-[>', '<Plug>(copilot-previous)')
+      --vim.keymap.set('i', '<C-]>', '<Plug>(copilot-next)')
+      --vim.keymap.set('i', '<C-[>', '<Plug>(copilot-previous)')
       vim.keymap.set('i', '<C-Space>', '<Plug>(copilot-suggest)')
     end
   }
