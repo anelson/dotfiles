7635ced90636fe0cece0affc69d89b24cd86c1b5
Adam Nelson <anelson@users.noreply.github.com>
2024-07-10 23:38:26 +0200

    [nvim] fix vim-tmux-navigator config so it actually works with lazy.

--- home/.config/nvim/lua/plugins/tmux.lua
+++ home/.config/nvim/lua/plugins/tmux.lua
@@ -1,10 +1,26 @@
 return {
   -- Use the same keys to navigate vim windows and tmux panes seamlessly
-  { "christoomey/vim-tmux-navigator" },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
 
   -- Use the contents of other tmux windows as a completion source. Supposedly
   -- this plugin automatically integrates with coc as a completion source
-  { "wellle/tmux-complete.vim" },
+  { "wellle/tmux-complete.vim", lazy = false },
 
   -- Use OSC-52 control codes to push the clipboard to the terminal client
   {
