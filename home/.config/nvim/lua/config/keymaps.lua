153c606cfb9c1c9033e658ccfae3a53256c8a559
Adam Nelson <anelson@users.noreply.github.com>
2024-07-10 16:51:27 +0200

    [nvim] Reset neovim config with mostly-stock LazyVim
    In this branch I'm experimenting with LazyVim as the basis for my
    config, to modernize things without a lot of effort and tinkering.  So
    far it's more frustrating than useful but I'm going to keep poking at
    it.
    
    Subsequent commits will consist of me applying my previous config on top
    of LazyVim.

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -1,41 +1,39 @@
-vim.g.mapleader = ","
-
--- TODO: these aren't needed in Neovim right?  They're on by default?
--- vim.cmd('filetype plugin indent on')
--- vim.cmd('syntax on') -- enable syntax highlighting
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
 
 -- Basic config settings
 
 -- Use jj instead of <ESC> to exit insert mode
 -- NOTE: I'm experimenting with using Caps Lock for this; maybe this will go
 -- away soon
-vim.keymap.set('i', 'jj', '<ESC>')
+vim.keymap.set("i", "jj", "<ESC>")
 
 -- Make a quick shortcut to hide the highlights from a search and close the
 -- preview window.  Yes they seem unrelated, but they're both annoying
 -- distractions
-vim.keymap.set('n', '<Esc><Esc>', ':nohlsearch<CR>:pclose<CR><ESC>', { noremap = true })
+vim.keymap.set("n", "<Esc><Esc>", ":nohlsearch<CR>:pclose<CR><ESC>", { noremap = true })
 
 -- Misc. shortcuts
 
 -- Make it easy to open my vimnotes file to note something
-vim.keymap.set('n', '<Leader>vn', ':split ~/.vim/vimnotes.txt<CR>', { noremap = true })
+vim.keymap.set("n", "<Leader>vn", ":split ~/.vim/vimnotes.txt<CR>", { noremap = true })
 
 -- Same for .vimrc
-vim.keymap.set('n', '<Leader>vc', ':split ~/.vimrc<CR>', { noremap = true })
+vim.keymap.set("n", "<Leader>vc", ":split ~/.vimrc<CR>", { noremap = true })
 
 -- Search for the word under the cursor in the whole project with grep
 -- A cheap poor mans 'find usages'
-vim.keymap.set('n', '<Leader>*', ':silent grep! "\\b<C-R><C-W>\\b"<CR>:cw<CR>', { noremap = true })
+vim.keymap.set("n", "<Leader>*", ':silent grep! "\\b<C-R><C-W>\\b"<CR>:cw<CR>', { noremap = true })
 
 -- Need a faster way to close a preview window
-vim.keymap.set('n', '<Leader>pc', ':pclose<CR>', { noremap = true })
+vim.keymap.set("n", "<Leader>pc", ":pclose<CR>", { noremap = true })
 
 -- Ditto for quickfix
-vim.keymap.set('n', '<Leader>qc', ':cclose<CR>', { noremap = true })
+vim.keymap.set("n", "<Leader>qc", ":cclose<CR>", { noremap = true })
 
 -- Fast saving (but only if there are unsaved changes)
-vim.keymap.set('n', '<Leader>s', ':update<CR>', { noremap = true })
+vim.keymap.set("n", "<Leader>s", ":update<CR>", { noremap = true })
 
 -- Folding
 vim.keymap.set("n", vim.g.mapleader .. "z", "za", {})
---
+++ home/.config/nvim/lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.g.mapleader = ","
+
+-- TODO: these aren't needed in Neovim right?  They're on by default?
+-- vim.cmd('filetype plugin indent on')
+-- vim.cmd('syntax on') -- enable syntax highlighting
+
+-- Enable the option to require a Prettier config file
+-- If no prettier config file is found, the formatter will not be used
+vim.g.lazyvim_prettier_needs_config = false
