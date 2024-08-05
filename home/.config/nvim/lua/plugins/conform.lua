0f65796fccfd93eb073322f7febc8cd6edc6efb2
Adam Nelson <anelson@users.noreply.github.com>
2024-08-05 21:50:07 +0200

    [nvim] change formatter config for markdown to use prettier
    The default `markdownlint-cli2` just doesn't work at all for me

--- home/.config/nvim/lua/plugins/conform.lua
+++ home/.config/nvim/lua/plugins/conform.lua
@@ -5,7 +5,21 @@ return {
       ---@type table<string, conform.FormatterUnit[]>
       formatters_by_ft = {
         protobuf = { "buf" },
+
+        -- `markdownlint-cli2` always fails with a timeout error when formatting.
+        -- Although prettier just doesn't do anything so that's not much better.
+        markdown = { "prettier" },
       },
+
+      -- This workaround suggested in https://github.com/stevearc/conform.nvim/issues/297 to address formatter timeout issues
+      -- I am unable to get Markdown formatted due to a timeout message
+      --
+      -- UPDATE: Doesn't work.  LazyVim complains that I should not set `format_on_save`, and it seems to be overwriding this setting.
+      -- Even an absurd timeout like 10 seconds doesn't work.  Doing a `gq` on Markdown immediately fails with a timeout error.
+      -- format_on_save = {
+      --   timeout_ms = 10000,
+      --   lsp_fallback = true,
+      -- },
     },
   },
 }
