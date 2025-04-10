a828d58cb00135f4e95fabeffcb072f3133a998b
Adam Nelson <anelson@users.noreply.github.com>
2025-04-10 20:16:25 +0200

    [nvim] Update copilot.lua config to use latest example config
    There was a refactor with breaking changes a few days ago.  I'm not sure
    what those were because the details were not documented, I just saw it
    in the commit log.  I updated this based on the current docs on the site
    for copilot.lua, hopefully it doesn't break or regress anything.

--- home/.config/nvim/lua/plugins/copilot.lua
+++ home/.config/nvim/lua/plugins/copilot.lua
@@ -3,36 +3,36 @@ return {
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
-    build = ":Copilot auth",
-    lazy = false,
-    opts = {
-      panel = {
-        enabled = false,
-      },
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        hide_during_completion = false,
-        keymap = {
-          -- <Tab> is mapped to accepting the copilot suggestion, but it's in keymaps.lua because
-          -- it's a bit more complex than just setting `accept` here.
-          accept = false,
-          accept_word = false,
-          accept_line = false,
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<C-]>",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        panel = {
+          enabled = false,
         },
-      },
-      filetypes = {
-        --Don't need to explicitly enable most file types, but copilot is by default disabled
-        --for these, but I want to re-enabled it.
-        yaml = true,
-        markdown = true,
-        help = true,
-        gitcommit = true,
-        gitrebase = true,
-      },
-    },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          hide_during_completion = false,
+          keymap = {
+            -- <Tab> is mapped to accepting the copilot suggestion, but it's in keymaps.lua because
+            -- it's a bit more complex than just setting `accept` here.
+            accept = false,
+            accept_word = false,
+            accept_line = false,
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+        filetypes = {
+          --Don't need to explicitly enable most file types, but copilot is by default disabled
+          --for these, but I want to re-enabled it.
+          yaml = true,
+          markdown = true,
+          gitcommit = true,
+          gitrebase = true,
+        },
+      })
+    end,
   },
 }
